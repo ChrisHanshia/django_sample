bd49ee2158288fb62277835a8e7be545b59179b9
ChrisHanshia16 <137986274+ChrisHanshia16@users.noreply.github.com>
2024-02-14 15:34:53 +0530

    Update views.py

--- summarize_video/views.py
+++ summarize_video/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponse
+from django.template import loader
 
-def members(request):    
-    return HttpResponse("Hello world!")
+def members(request):
+  template = loader.get_template('index.html')
+  return HttpResponse(template.render())
